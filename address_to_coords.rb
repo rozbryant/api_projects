4e788aa41080256acd952c224f9ec683e69f7cac
Roz Bryant <rozbryant@gmail.com>
2014-10-28 23:42:43 -0500

    push address_to_coords.rb

--- address_to_coords.rb
+++ address_to_coords.rb
@@ -13,22 +13,25 @@ puts "What is the address you would like to find the latitude and longitude of?"
 the_address = gets.chomp
 
 # Replaces spaces and other URL-illegal characters in the string
-url_safe_address = URI.encode(the_address)
+url = URI.encode(the_address)
 
 # Your code goes below. Hints:
 
-# url_of_data_we_want = "???"
-# raw_data = ???
-# parsed_data = ???
-
+url = "http://maps.googleapis.com/maps/api/geocode/json?address=5807+S+Woodlawn+Ave+chicago+IL+60637"
+raw_data = open(url).read
+parsed_data = JSON.parse(raw_data)
+results = parsed_data["results"]
+first = results[0]
+geometry = first["geometry"]
+location = geometry["location"]
 # ...
 
 # Let's store the latitude in a variable called 'the_latitude',
 #   and the longitude in a variable called 'the_longitude'.
 
-# the_latitude = ???
-# the_longitude = ???
+the_latitude = location["lat"]
+the_longitude = location["lng"]
 
-# Ultimately, we want the following line to work when uncommented:
+#Ultimately, we want the following line to work when uncommented:
 
-# puts "The latitude of #{the_address} is #{the_latitude} and the longitude is #{the_longitude}."
+puts "The latitude of #{the_address} is #{the_latitude} and the longitude is #{the_longitude}."
