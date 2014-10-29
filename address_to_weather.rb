b21608ff3bf8edb5026d9b9f4f259913f371d826
Roz Bryant <rozbryant@gmail.com>
2014-10-28 23:52:27 -0500

    push address_to_weather.rb

--- address_to_weather.rb
+++ address_to_weather.rb
@@ -1,5 +1,7 @@
 require 'open-uri'
 require 'json'
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 # If you experience an error relating to SSL,
 #   uncomment the following two lines:
@@ -11,9 +13,13 @@ puts "Let's get the weather forecast for your address."
 
 puts "What is the address you would like to know the weather for?"
 the_address = gets.chomp
-url_safe_address = URI.encode(the_address)
+url = URI.encode(the_address)
 
 # Your code goes below.
+url = "http://api.forecast.io/forecast/8687fce2302d4f90e2c59ca819be9892/41.4896234,-87.5964137"
+raw_data = open(url).read
+parsed_data = JSON.parse(raw_data)
+
 
 # Ultimately, we want the following line to work when uncommented:
 
